dc22e58445dde867e6e59ed873f7c043805f956b
dalu <guodalu@qq.com>
2017-01-01 20:17:37 +0800

    fix event

--- example/mvc/home/Controller/Index.php
+++ example/mvc/home/Controller/Index.php
@@ -8,13 +8,6 @@ class Index extends Init
     function index()
     {
 
-        echo $this->querier->select('mysql')
-            ->from('user', array('id', 'name', 'password', 'age'))
-            ->joinLeft(array('user_detail'), 'user.id=user_detail.uid', array('account'))
-            ->where('id=?', '123');
-
-//        echo '欢迎使用Ark框架,有问题请咨询QQ:112183883';
-
     }
 
 }
