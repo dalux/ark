7ae74156309b260d82ca919d8a497313141109f1
dalu <guodalu@qq.com>
2017-01-18 01:06:26 +0800

    fix example

--- example/app/home/controller/index.php
+++ example/app/home/controller/index.php
@@ -12,15 +12,7 @@ class Index extends Sailor
 
     function index()
     {
-        $data = Querier::select('oci')
-            ->from(array('a'=> 'bm_category_2017'), array('*'))
-            ->where('a.pid>:pid', 1)
-            ->where('a.cid<:cid', 5)
-            ->limit(10)
-            ->invoke($this->db)
-            ->cache(86400, 'bm_category_2017_1', $this->cache)
-            ->fetchAll();
-        Toolkit::showPrint($data, Trace::get('database'));
+
     }
 
 }
