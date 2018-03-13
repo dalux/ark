b5dc83f83969452e962bb59575d8ec739c8760c8
dalu <guodalu@qq.com>
2018-03-13 22:30:05 +0800

    fix

--- src/Router/Native.php
+++ src/Router/Native.php
@@ -1,6 +1,6 @@
 <?php
 
-class Ark_Router_Native extends Ark_Router_Father
+class Ark_Router_Multiple extends Ark_Router_Father
 {
 
     /**
@@ -11,18 +11,11 @@ class Ark_Router_Native extends Ark_Router_Father
     private $_controller;
 
     /**
-     * 路由是否就绪
+     * 路由状态
      *
      * @var bool
      */
     private $_ready = false;
-
-    /**
-     * 路由转换规则
-     *
-     * @var array
-     */
-    private $_rules = array();
 
     /**
      * 获取控制器名称
@@ -35,22 +28,9 @@ class Ark_Router_Native extends Ark_Router_Father
     }
 
     /**
-     * 添加路由规则
-     *
-     * @param string $rule
-     * @param string $redirect
-     * @return $this
-     */
-    function addRule($rule, $redirect)
-    {
-        $this->_rules[$rule] = $redirect;
-        return $this;
-    }
-
-    /**
      * 路由是否就绪
      *
-     * @return bool
+     * @return mixed
      */
     function isReady()
     {
@@ -58,86 +38,21 @@ class Ark_Router_Native extends Ark_Router_Father
     }
 
     /**
-     * 准备路由数据
+     * 准备请求数据
      *
-     * @throws Exception
+     * @return null
      */
-    function ready()
-    {
-        $uri = $_SERVER['REQUEST_URI'];
-        //解析URI
-        if (strpos($uri, '?') !== false) {
-            $uri = substr($uri, 0, strpos($uri, '?'));
-        }
-        //重写
-        $uri = trim($this->_rewrite($uri), '/');
-        //处理URI,组装控制器类
-        $urlsep = Ark_Core::getInstance()->config->router->urlsep;
-        $url_suffix = Ark_Core::getInstance()->config->router->urlsuffix;
-        if (strpos($uri, $url_suffix) !== false) {
-            $uri = preg_replace(sprintf('~%s$~i', $url_suffix), '', $uri);
-        }
-        $controller_dir = Ark_Core::getInstance()->getControllerPath();
-        $path_now = $controller_dir;
-        if ($uri == '') {
-            $controller = Ark_Core::getInstance()->config->router->controller;
-            $path_now.= DIRECTORY_SEPARATOR. $controller;
-        } else {
-            $controllers = array_map('strtolower', explode($urlsep, $uri));
-            $controller = implode(DIRECTORY_SEPARATOR, $controllers);
-            $path_now.= DIRECTORY_SEPARATOR. $controller;
-        }
-        $this->_controller = $path_now;
-        //定义PATH_NOW常量
-        defined('PATH_NOW') || define('PATH_NOW', dirname($path_now));
-        Ark_Loader::setAlias('~', PATH_NOW);
+	function ready()
+	{
+        //定义常量
+        $debug_trace = debug_backtrace();
+        $caller = current(end($debug_trace));
+        define('PATH_NOW', dirname($caller));
+        $this->_controller = $caller;
         //请求数据初始化完成
         Ark_Request::setReady(true);
         Ark_Core::getInstance()->setMember('request', function() { return Ark_Request::getInstance(); });
         $this->_ready = true;
-    }
-
-    /**
-     * 调度
-     *
-     * @throws Exception
-     */
-    function dispatch()
-    {
-        parent::dispatch();
-        $output = include_once($this->_controller);
-        if (is_string($output)) {
-            echo $output;
-        }
-    }
-
-    /**
-     * 重写URL规则
-     *
-     * @param $uri
-     * @return mixed
-     * @throws Ark_Router_Exception
-     */
-    private function _rewrite($uri)
-    {
-        foreach ($this->_rules as $key=> $val) {
-            $key = '~'. $key. '~i';
-            if (preg_match($key, $uri)) {
-                if (is_string($val)) {
-                    $uri = preg_replace($key, $val, $uri);
-                    break;
-                } elseif (is_callable($val)) {
-                    $uri = preg_replace_callback($key, $val, $uri);
-                    break;
-                } elseif (!is_callable($val) && is_array($val)) {
-                    throw new Ark_Router_Exception(Ark_Core::getInstance()->lang->get('router.call_func_failed', $val[0]. '::'. $val[1]. '()'));
-                }
-            }
-        }
-        if (!is_string($uri)) {
-            throw new Ark_Router_Exception(Ark_Core::getInstance()->lang->get('router.uri_must_string'));
-        }
-        return $uri;
-    }
+	}
 
 }
