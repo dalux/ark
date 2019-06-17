23b9c3755b86d2831d96c11f347683fd7be40376
dalux <guodalu@qq.com>
2019-06-18 00:05:03 +0800

    fix

---
+++ src/Toolkit/File.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Brisk\Toolkit;
+
+class File
+{
+
+    /**
+     * Recursively create a directory
+     *
+     * @param string $dir
+     * @pram bool $mode
+     * @return bool
+     */
+    public static function mkDir(string $dir, int $mode = 0755)
+    {
+		$dir = trim($dir);
+        if (!$dir = trim($dir)) {
+            return false;
+        }
+        $dirs = [$dir];
+        while (!file_exists($dir)) {
+            $dir = dirname($dir);
+            $dirs[] = $dir;
+        }
+        if (count($dirs)>1) {
+            $dirs = array_reverse($dirs);
+            foreach ($dirs as $k=> $v) {
+                $v = rtrim($v, DIRECTORY_SEPARATOR);
+                if (!file_exists($v) && !mkdir($v, $mode)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Recursively copy a directory
+     *
+     * @param string $src
+     * @param string $dst
+     * @return bool
+     */
+    public static function copyDir(string $src, string $dst)
+    {
+        if (is_dir($src)) {
+            $handle = opendir($src);
+            self::mkDir($dst);
+            $sep = DIRECTORY_SEPARATOR;
+            while (true) {
+                $file = readdir($handle);
+                if ($file == false) {
+                    break;
+                }
+                if (($file != '.') && ($file != '..')) {
+                    if (is_dir($src. $sep. $file)) {
+                        self::copyDir($src. $sep. $file, $dst. $sep. $file);
+                    } else {
+                        copy($src. $sep. $file, $dst. $sep. $file);
+                    }
+                }
+            }
+            closedir($handle);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Recursively remove a directory
+     *
+     * @param string $dir_name
+     * @return bool
+     */
+	public static function removeDir(string $dir_name)
+	{
+		if (is_dir($dir_name)) {
+			$handle = opendir($dir_name);
+			while(true) {
+				$file = readdir($handle);
+				if ($file == false) {
+					break;
+				}
+				if (!in_array($file, ['.', '..'])) {
+					$dir = $dir_name. '/' . $file;
+					if (is_dir($dir)) {
+						self::removeDir($dir);
+					} else {
+						unlink($dir);
+					}
+				}
+			}
+			closedir($handle);
+			return rmdir($dir_name);
+		}
+		return true;
+    }
+
+}
---
+++ src/Toolkit/Verifier.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Brisk\Toolkit;
+
+class Verifier
+{
+
+    /**
+     * 数据校验器
+     *
+     * @param array $data
+     * @param array $rule
+     */
+    public static function check(array $data, array $rule)
+    {
+        foreach ($rule as $field=> $item) {
+            $value = $data[$field];
+            foreach ($item as $k=> $v) {
+                //必填项校验
+                if ($k == 'required' && $v) {
+                    if (is_string($v)) {
+                        $special = [];
+                        foreach ($rule as $_k=> $_v) {
+                            if ($_v['required'] === $v) {
+                                $special[$_k] = $_v;
+                            }
+                        }
+                        $pass = false;
+                        foreach ($special as $_k=> $_v) {
+                            $_value = $data[$_k];
+                            if ($_value || is_integer($_value)) {
+                                $pass = true;
+                            }
+                        }
+                        if (!$pass) {
+                            $msg = Language::format('tool.validator_field_missing', $field);
+                            throw new RuntimeException($msg);
+                        }
+                    } elseif (is_null($value)
+                            || is_string($value) && trim($value) == ''
+                            || is_array($value) && count($value) == 0) {
+                        $msg = Language::format('tool.validator_field_missing', $field);
+                        throw new RuntimeException($msg);
+                    }
+                }
+                //以下校验在值不为空时进行
+                if (!is_null($value)) {
+                    //类型校验
+                    if ($k == 'type') {
+                        $type = strtolower(gettype($value));
+                        $v = strtolower($v);
+                        $number = ['double', 'float', 'real', 'integer', 'int'];
+                        if (in_array($type, $number)
+                                || $type == 'string' && $v == 'number' && is_numeric($value)) {
+                            $type = 'number';
+                        } elseif ($type == 'resource') {
+                            $type = 'object';
+                        }
+                        if ($v != $type) {
+                            $msg = Language::format('tool.validator_field_errtype', $field, $v, $type);
+                            throw new RuntimeException($msg);
+                        }
+                    }
+                    //范围校验
+                    if ($k == 'range' && is_array($v)) {
+                        if (!in_array($value, $v)) {
+                            $msg = Language::format('tool.validator_field_outrange', $field);
+                            throw new RuntimeException($msg);
+                        }
+                    }
+                    //最小值校验
+                    if ($k == 'min_value' && $value < $v) {
+                        $msg = Language::format('tool.validator_filed_lessthan', $field, $v);
+                        throw new RuntimeException($msg);
+                    }
+                    //最大值校验
+                    if ($k == 'max_value' && $value > $v) {
+                        $msg = Language::format('tool.validator_filed_morethan', $field, $v);
+                        throw new RuntimeException($msg);
+                    }
+                    //最小长度校验
+                    if ($k == 'min_length' && is_string($value) && strlen($value) < $v) {
+                        $msg = Language::format('tool.validator_filed_minlength', $field, $v, strlen($value));
+                        throw new RuntimeException($msg);
+                    }
+                    //最大长度校验
+                    if ($k == 'max_length' && is_string($value) && strlen($value) > $v) {
+                        $msg = Language::format('tool.validator_filed_maxlength', $field, $v, strlen($value));
+                        throw new RuntimeException($msg);
+                    }
+                    //是否相等校验
+                    if ($k == 'equal') {
+                        if (is_string($value) && $value != $v) {
+                            $msg = Language::format('tool.validator_filed_noequal', $field, $v, $value);
+                            throw new RuntimeException($msg);
+                        } elseif (is_object($value) && !$value instanceof $v) {
+                            $msg = Language::format('tool.validator_filed_errobject', $field, get_class($v));
+                            throw new RuntimeException($msg);
+                        }
+                    }
+                    //是否匹配规则
+                    if ($k == 'match' && !preg_match($v, $value)) {
+                        $msg = Language::format('tool.validator_filed_nomatch', $field, $v);
+                        throw new RuntimeException($msg);
+                    }
+                }
+            }
+        }
+    }
+
+}
